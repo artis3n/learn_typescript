19aec98fac39f31fd39c7937f06d9b1a13d138d9
Artis3n <dev@artis3nal.com>
2020-01-26 20:39:41 -0500

    Tuples

--- section2/app.js
+++ section2/app.js
@@ -1,13 +1,12 @@
-function add(n1, n2, showResult, phrase) {
-    if (showResult) {
-        console.log(phrase, n1 + n2);
-    }
-    else {
-        return n1 + n2;
-    }
+var person = {
+    name: 'Ari',
+    age: 25,
+    hobbies: ['Sports', 'Cooking']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is:';
-add(number1, number2, printResult, resultPhrase);
--- section2/app.ts
+++ section2/app.ts
@@ -1,14 +1,23 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-    if (showResult) {
-        console.log(phrase, n1 + n2);
-    } else {
-        return n1 + n2;
-    }
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string]
+} = {
+    name: 'Ari',
+    age: 25,
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author']
+};
+
+person.role.push('admin');
+person.role[1] = 10;
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
 }
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is:';
-
-add(number1, number2, printResult, resultPhrase);
